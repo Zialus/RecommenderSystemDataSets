ea65eda60d807434099ea91900e9ee954e3de1d2
Raul Ferreira <raulmendesferreira@gmail.com>
2018-11-21 18:19:34 +0000

    Small adjustments to scripts and travis config.

--- ml10M/prepare_ml10M_data.py
+++ ml10M/prepare_ml10M_data.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
 # In[1]:
@@ -9,24 +9,65 @@ from six.moves import urllib
 import numpy as np
 from scipy import sparse
 from sklearn.model_selection import train_test_split
+import zipfile as zf
 
 
 # In[2]:
 
 
-get_ipython().run_cell_magic('time', '', '# Download the data.\nurl = \'http://files.grouplens.org/datasets/movielens/\'\n\n\ndef maybe_download(filename, expected_bytes):\n    """Download a file if not present, and make sure it\'s the right size."""\n    if not os.path.exists(filename):\n        filename, _ = urllib.request.urlretrieve(url + filename, filename)\n    statinfo = os.stat(filename)\n    if statinfo.st_size == expected_bytes:\n        print(\'Found and verified\', filename)\n    else:\n        print(statinfo.st_size)\n        raise Exception(\'Failed to verify \' + filename + \'. Can you get to it with a browser?\')\n    return filename\n\n\ndata_file = maybe_download(\'ml-10m.zip\', 65566137)')
+# Download the data.
+url = 'http://files.grouplens.org/datasets/movielens/'
+
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception('Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+data_file = maybe_download('ml-10m.zip', 65566137)
 
 
 # In[3]:
 
 
-get_ipython().run_cell_magic('time', '', "get_ipython().system(u'unzip -o ml-10m.zip')")
+with zf.ZipFile("ml-10m.zip", "r") as zip_ref:
+    zip_ref.extractall()
 
 
 # In[4]:
 
 
-get_ipython().run_cell_magic('time', '', "# file should look like\n'''\n1::122::5::838985046\n1::185::5::838983525\n1::231::5::838983392\n1::292::5::838983421\n1::316::5::838983392\n1::329::5::838983392\n1::355::5::838984474\n1::356::5::838983653\n1::362::5::838984885\n1::364::5::838983707\n'''\nm = 71567\nn = 65133\nnnz_train = 9000048\nnnz_test = 1000006\n\ndata_filename = 'ml-10M100K/ratings.dat'\n\nuser, item, rating = np.loadtxt(data_filename, delimiter='::',\n                                dtype=[('f0', np.int32), ('f1', np.int32), ('f2', np.float)],\n                                unpack=True)")
+# file should look like
+'''
+1::122::5::838985046
+1::185::5::838983525
+1::231::5::838983392
+1::292::5::838983421
+1::316::5::838983392
+1::329::5::838983392
+1::355::5::838984474
+1::356::5::838983653
+1::362::5::838984885
+1::364::5::838983707
+'''
+m = 71567
+n = 65133
+nnz_train = 9000048
+nnz_test = 1000006
+
+data_filename = 'ml-10M100K/ratings.dat'
+
+user, item, rating = np.loadtxt(data_filename, delimiter='::',
+                                dtype=[('f0', np.int32), ('f1', np.int32), ('f2', np.float)],
+                                unpack=True)
 
 
 # In[5]:
@@ -58,19 +99,37 @@ assert user.size == nnz_train + nnz_test
 # In[6]:
 
 
-get_ipython().run_cell_magic('time', '', 'user_item = np.vstack((user, item))\n\nuser_item_train, user_item_test, rating_train, rating_test = train_test_split(user_item.T,\n                                                                              rating,\n                                                                              test_size=nnz_test,\n                                                                              random_state=42)')
+user_item = np.vstack((user, item))
+
+user_item_train, user_item_test, rating_train, rating_test = train_test_split(user_item.T,
+                                                                              rating,
+                                                                              test_size=nnz_test,
+                                                                              random_state=42)
 
 
 # In[7]:
 
 
-get_ipython().run_cell_magic('time', '', 'R_test_coo = sparse.coo_matrix((rating_test, (user_item_test[:, 0], user_item_test[:, 1])))\nassert R_test_coo.nnz == nnz_test\n\noutfile_test = open("test.txt", \'w\')\nfor i in range(nnz_test):\n    outfile_test.write(str((user_item_test[i, 0])) + " " + str((user_item_test[i, 1])) + " " + str(rating_test[i]) + "\\n")')
+R_test_coo = sparse.coo_matrix((rating_test, (user_item_test[:, 0], user_item_test[:, 1])))
+assert R_test_coo.nnz == nnz_test
+
+outfile_test = open("test.txt", 'w')
+for i in range(nnz_test):
+    outfile_test.write(str((user_item_test[i, 0])) + " " + str((user_item_test[i, 1])) + " " + str(rating_test[i]) + "\n")
 
 
 # In[8]:
 
 
-get_ipython().run_cell_magic('time', '', "# for test data, we need COO format to calculate test RMSE\n\nR_test_coo.data.astype(np.float32).tofile('R_test_coo.data.bin')\nR_test_coo.row.tofile('R_test_coo.row.bin')\nR_test_coo.col.tofile('R_test_coo.col.bin')\n\ntest_data = np.fromfile('R_test_coo.data.bin', dtype=np.float32)\ntest_row = np.fromfile('R_test_coo.row.bin', dtype=np.int32)\ntest_col = np.fromfile('R_test_coo.col.bin', dtype=np.int32)")
+# for test data, we need COO format to calculate test RMSE
+
+R_test_coo.data.astype(np.float32).tofile('R_test_coo.data.bin')
+R_test_coo.row.tofile('R_test_coo.row.bin')
+R_test_coo.col.tofile('R_test_coo.col.bin')
+
+test_data = np.fromfile('R_test_coo.data.bin', dtype=np.float32)
+test_row = np.fromfile('R_test_coo.row.bin', dtype=np.int32)
+test_col = np.fromfile('R_test_coo.col.bin', dtype=np.int32)
 
 
 # In[9]:
@@ -105,19 +164,49 @@ print(np.unique(R_test_coo.col).size)
 # In[11]:
 
 
-get_ipython().run_cell_magic('time', '', 'R_train_coo = sparse.coo_matrix((rating_train, (user_item_train[:, 0], user_item_train[:, 1])))\nassert R_train_coo.nnz == nnz_train\n\noutfile_train = open("train.txt", \'w\')\nfor i in range(nnz_train):\n    outfile_train.write(str((user_item_train[i, 0])) + " " + str((user_item_train[i, 1])) + " " + str(rating_train[i]) + "\\n")')
+R_train_coo = sparse.coo_matrix((rating_train, (user_item_train[:, 0], user_item_train[:, 1])))
+assert R_train_coo.nnz == nnz_train
+
+outfile_train = open("train.txt", 'w')
+for i in range(nnz_train):
+    outfile_train.write(str((user_item_train[i, 0])) + " " + str((user_item_train[i, 1])) + " " + str(rating_train[i]) + "\n")
 
 
 # In[12]:
 
 
-get_ipython().run_cell_magic('time', '', "# for training data, we need COO format to calculate training RMSE\n# we need CSR format R when calculate X from \\Theta\n# we need CSC format of R when calculating \\Theta from X\nR_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')\nR_train_coo.row.tofile('R_train_coo.row.bin')\nR_train_coo.col.tofile('R_train_coo.col.bin')\n\nR_train_csr = R_train_coo.tocsr()\nR_train_csc = R_train_coo.tocsc()\n\nR_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')\nR_train_csr.indices.tofile('R_train_csr.indices.bin')\nR_train_csr.indptr.tofile('R_train_csr.indptr.bin')\nR_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')\nR_train_csc.indices.tofile('R_train_csc.indices.bin')\nR_train_csc.indptr.tofile('R_train_csc.indptr.bin')")
+# for training data, we need COO format to calculate training RMSE
+# we need CSR format R when calculate X from \Theta
+# we need CSC format of R when calculating \Theta from X
+R_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')
+R_train_coo.row.tofile('R_train_coo.row.bin')
+R_train_coo.col.tofile('R_train_coo.col.bin')
+
+R_train_csr = R_train_coo.tocsr()
+R_train_csc = R_train_coo.tocsc()
+
+R_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')
+R_train_csr.indices.tofile('R_train_csr.indices.bin')
+R_train_csr.indptr.tofile('R_train_csr.indptr.bin')
+R_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')
+R_train_csc.indices.tofile('R_train_csc.indices.bin')
+R_train_csc.indptr.tofile('R_train_csc.indptr.bin')
 
 
 # In[13]:
 
 
-get_ipython().run_cell_magic('time', '', "train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)\ntrain_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)\ntrain_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)\n\ntrain_csc_data = np.fromfile('R_train_csc.data.bin', dtype=np.float32)\ntrain_csc_indices = np.fromfile('R_train_csc.indices.bin', dtype=np.int32)\ntrain_csc_indptr = np.fromfile('R_train_csc.indptr.bin', dtype=np.int32)\n\ntrain_csr_data = np.fromfile('R_train_csr.data.bin', dtype=np.float32)\ntrain_csr_indices = np.fromfile('R_train_csr.indices.bin', dtype=np.int32)\ntrain_csr_indptr = np.fromfile('R_train_csr.indptr.bin', dtype=np.int32)")
+train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)
+train_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)
+train_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)
+
+train_csc_data = np.fromfile('R_train_csc.data.bin', dtype=np.float32)
+train_csc_indices = np.fromfile('R_train_csc.indices.bin', dtype=np.int32)
+train_csc_indptr = np.fromfile('R_train_csc.indptr.bin', dtype=np.int32)
+
+train_csr_data = np.fromfile('R_train_csr.data.bin', dtype=np.float32)
+train_csr_indices = np.fromfile('R_train_csr.indices.bin', dtype=np.int32)
+train_csr_indptr = np.fromfile('R_train_csr.indptr.bin', dtype=np.int32)
 
 
 # In[14]:
@@ -168,11 +257,30 @@ print(np.unique(R_train_coo.col).size)
 # In[16]:
 
 
-get_ipython().run_cell_magic('time', '', 'print("writing extra meta_modified_all file")\n\noutfile_meta = open("meta_modified_all", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n" + str(nnz_train) + "\\n")\noutfile_meta.write("""R_train_coo.data.bin\nR_train_coo.row.bin\nR_train_coo.col.bin\nR_train_csr.indptr.bin\nR_train_csr.indices.bin\nR_train_csr.data.bin\nR_train_csc.indptr.bin\nR_train_csc.indices.bin\nR_train_csc.data.bin\n""")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
+print("writing extra meta_modified_all file")
+
+outfile_meta = open("meta_modified_all", 'w')
+outfile_meta.write(str(m) + " " + str(n) + "\n" + str(nnz_train) + "\n")
+outfile_meta.write("""R_train_coo.data.bin
+R_train_coo.row.bin
+R_train_coo.col.bin
+R_train_csr.indptr.bin
+R_train_csr.indices.bin
+R_train_csr.data.bin
+R_train_csc.indptr.bin
+R_train_csc.indices.bin
+R_train_csc.data.bin
+""")
+outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
 
 
 # In[17]:
 
 
-get_ipython().run_cell_magic('time', '', 'print("writing extra meta file")\n\noutfile_meta = open("meta", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n")\noutfile_meta.write(str(nnz_train) + " " + "train.txt\\n")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
+print("writing extra meta file")
+
+outfile_meta = open("meta", 'w')
+outfile_meta.write(str(m) + " " + str(n) + "\n")
+outfile_meta.write(str(nnz_train) + " " + "train.txt\n")
+outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
 
--- ml20M/prepare_ml20M_data.py
+++ ml20M/prepare_ml20M_data.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
 # In[1]:
@@ -10,24 +10,67 @@ import numpy as np
 import pandas as pd
 from scipy import sparse
 from sklearn.model_selection import train_test_split
+import zipfile as zf
 
 
 # In[2]:
 
 
-get_ipython().run_cell_magic('time', '', '# Download the data.\nurl = \'http://files.grouplens.org/datasets/movielens/\'\n\n\ndef maybe_download(filename, expected_bytes):\n    """Download a file if not present, and make sure it\'s the right size."""\n    if not os.path.exists(filename):\n        filename, _ = urllib.request.urlretrieve(url + filename, filename)\n    statinfo = os.stat(filename)\n    if statinfo.st_size == expected_bytes:\n        print(\'Found and verified\', filename)\n    else:\n        print(statinfo.st_size)\n        raise Exception(\'Failed to verify \' + filename + \'. Can you get to it with a browser?\')\n    return filename\n\n\ndata_file = maybe_download(\'ml-20m.zip\', 198702078)')
+# Download the data.
+url = 'http://files.grouplens.org/datasets/movielens/'
+
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception('Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+data_file = maybe_download('ml-20m.zip', 198702078)
 
 
 # In[3]:
 
 
-get_ipython().run_cell_magic('time', '', "get_ipython().system(u'unzip -o ml-20m.zip')")
+with zf.ZipFile("ml-20m.zip", "r") as zip_ref:
+    zip_ref.extractall()
 
 
 # In[4]:
 
 
-get_ipython().run_cell_magic('time', '', "# file should look like\n'''\nuserId,movieId,rating,timestamp\n1,2,3.5,1112486027\n1,29,3.5,1112484676\n1,32,3.5,1112484819\n1,47,3.5,1112484727\n1,50,3.5,1112484580\n1,112,3.5,1094785740\n1,151,4.0,1094785734\n1,223,4.0,1112485573\n1,253,4.0,1112484940\n'''\nm = 138493\nn = 131262\nnnz_train = 18000236\nnnz_test = 2000027\n\ndata_filename = 'ml-20m/ratings.csv'\n\ndata = pd.read_csv(data_filename, dtype={0:'int32',1:'int32',2:'float32'},usecols=[0,1,2])\n\nuser = data['userId'].values\nitem = data['movieId'].values\nrating = data['rating'].values")
+# file should look like
+'''
+userId,movieId,rating,timestamp
+1,2,3.5,1112486027
+1,29,3.5,1112484676
+1,32,3.5,1112484819
+1,47,3.5,1112484727
+1,50,3.5,1112484580
+1,112,3.5,1094785740
+1,151,4.0,1094785734
+1,223,4.0,1112485573
+1,253,4.0,1112484940
+'''
+m = 138493
+n = 131262
+nnz_train = 18000236
+nnz_test = 2000027
+
+data_filename = 'ml-20m/ratings.csv'
+
+data = pd.read_csv(data_filename, dtype={0: 'int32', 1: 'int32', 2: 'float32'}, usecols=[0, 1, 2])
+
+user = data['userId'].values
+item = data['movieId'].values
+rating = data['rating'].values
 
 
 # In[5]:
@@ -59,19 +102,37 @@ assert user.size == nnz_train + nnz_test
 # In[6]:
 
 
-get_ipython().run_cell_magic('time', '', 'user_item = np.vstack((user, item))\n\nuser_item_train, user_item_test, rating_train, rating_test = train_test_split(user_item.T,\n                                                                              rating,\n                                                                              test_size=nnz_test,\n                                                                              random_state=42)')
+user_item = np.vstack((user, item))
+
+user_item_train, user_item_test, rating_train, rating_test = train_test_split(user_item.T,
+                                                                              rating,
+                                                                              test_size=nnz_test,
+                                                                              random_state=42)
 
 
 # In[7]:
 
 
-get_ipython().run_cell_magic('time', '', 'R_test_coo = sparse.coo_matrix((rating_test, (user_item_test[:, 0], user_item_test[:, 1])))\nassert R_test_coo.nnz == nnz_test\n\noutfile_test = open("test.txt", \'w\')\nfor i in range(nnz_test):\n    outfile_test.write(str((user_item_test[i, 0])) + " " + str((user_item_test[i, 1])) + " " + str(rating_test[i]) + "\\n")')
+R_test_coo = sparse.coo_matrix((rating_test, (user_item_test[:, 0], user_item_test[:, 1])))
+assert R_test_coo.nnz == nnz_test
+
+outfile_test = open("test.txt", 'w')
+for i in range(nnz_test):
+    outfile_test.write(str((user_item_test[i, 0])) + " " + str((user_item_test[i, 1])) + " " + str(rating_test[i]) + "\n")
 
 
 # In[8]:
 
 
-get_ipython().run_cell_magic('time', '', "# for test data, we need COO format to calculate test RMSE\n\nR_test_coo.data.astype(np.float32).tofile('R_test_coo.data.bin')\nR_test_coo.row.tofile('R_test_coo.row.bin')\nR_test_coo.col.tofile('R_test_coo.col.bin')\n\ntest_data = np.fromfile('R_test_coo.data.bin', dtype=np.float32)\ntest_row = np.fromfile('R_test_coo.row.bin', dtype=np.int32)\ntest_col = np.fromfile('R_test_coo.col.bin', dtype=np.int32)")
+# for test data, we need COO format to calculate test RMSE
+
+R_test_coo.data.astype(np.float32).tofile('R_test_coo.data.bin')
+R_test_coo.row.tofile('R_test_coo.row.bin')
+R_test_coo.col.tofile('R_test_coo.col.bin')
+
+test_data = np.fromfile('R_test_coo.data.bin', dtype=np.float32)
+test_row = np.fromfile('R_test_coo.row.bin', dtype=np.int32)
+test_col = np.fromfile('R_test_coo.col.bin', dtype=np.int32)
 
 
 # In[9]:
@@ -106,19 +167,49 @@ print(np.unique(R_test_coo.col).size)
 # In[11]:
 
 
-get_ipython().run_cell_magic('time', '', 'R_train_coo = sparse.coo_matrix((rating_train, (user_item_train[:, 0], user_item_train[:, 1])))\nassert R_train_coo.nnz == nnz_train\n\noutfile_train = open("train.txt", \'w\')\nfor i in range(nnz_train):\n    outfile_train.write(str((user_item_train[i, 0])) + " " + str((user_item_train[i, 1])) + " " + str(rating_train[i]) + "\\n")')
+R_train_coo = sparse.coo_matrix((rating_train, (user_item_train[:, 0], user_item_train[:, 1])))
+assert R_train_coo.nnz == nnz_train
+
+outfile_train = open("train.txt", 'w')
+for i in range(nnz_train):
+    outfile_train.write(str((user_item_train[i, 0])) + " " + str((user_item_train[i, 1])) + " " + str(rating_train[i]) + "\n")
 
 
 # In[12]:
 
 
-get_ipython().run_cell_magic('time', '', "# for training data, we need COO format to calculate training RMSE\n# we need CSR format R when calculate X from \\Theta\n# we need CSC format of R when calculating \\Theta from X\nR_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')\nR_train_coo.row.tofile('R_train_coo.row.bin')\nR_train_coo.col.tofile('R_train_coo.col.bin')\n\nR_train_csr = R_train_coo.tocsr()\nR_train_csc = R_train_coo.tocsc()\n\nR_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')\nR_train_csr.indices.tofile('R_train_csr.indices.bin')\nR_train_csr.indptr.tofile('R_train_csr.indptr.bin')\nR_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')\nR_train_csc.indices.tofile('R_train_csc.indices.bin')\nR_train_csc.indptr.tofile('R_train_csc.indptr.bin')")
+# for training data, we need COO format to calculate training RMSE
+# we need CSR format R when calculate X from \Theta
+# we need CSC format of R when calculating \Theta from X
+R_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')
+R_train_coo.row.tofile('R_train_coo.row.bin')
+R_train_coo.col.tofile('R_train_coo.col.bin')
+
+R_train_csr = R_train_coo.tocsr()
+R_train_csc = R_train_coo.tocsc()
+
+R_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')
+R_train_csr.indices.tofile('R_train_csr.indices.bin')
+R_train_csr.indptr.tofile('R_train_csr.indptr.bin')
+R_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')
+R_train_csc.indices.tofile('R_train_csc.indices.bin')
+R_train_csc.indptr.tofile('R_train_csc.indptr.bin')
 
 
 # In[13]:
 
 
-get_ipython().run_cell_magic('time', '', "train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)\ntrain_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)\ntrain_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)\n\ntrain_csc_data = np.fromfile('R_train_csc.data.bin', dtype=np.float32)\ntrain_csc_indices = np.fromfile('R_train_csc.indices.bin', dtype=np.int32)\ntrain_csc_indptr = np.fromfile('R_train_csc.indptr.bin', dtype=np.int32)\n\ntrain_csr_data = np.fromfile('R_train_csr.data.bin', dtype=np.float32)\ntrain_csr_indices = np.fromfile('R_train_csr.indices.bin', dtype=np.int32)\ntrain_csr_indptr = np.fromfile('R_train_csr.indptr.bin', dtype=np.int32)")
+train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)
+train_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)
+train_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)
+
+train_csc_data = np.fromfile('R_train_csc.data.bin', dtype=np.float32)
+train_csc_indices = np.fromfile('R_train_csc.indices.bin', dtype=np.int32)
+train_csc_indptr = np.fromfile('R_train_csc.indptr.bin', dtype=np.int32)
+
+train_csr_data = np.fromfile('R_train_csr.data.bin', dtype=np.float32)
+train_csr_indices = np.fromfile('R_train_csr.indices.bin', dtype=np.int32)
+train_csr_indptr = np.fromfile('R_train_csr.indptr.bin', dtype=np.int32)
 
 
 # In[14]:
@@ -169,11 +260,30 @@ print(np.unique(R_train_coo.col).size)
 # In[16]:
 
 
-get_ipython().run_cell_magic('time', '', 'print("writing extra meta_modified_all file")\n\noutfile_meta = open("meta_modified_all", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n" + str(nnz_train) + "\\n")\noutfile_meta.write("""R_train_coo.data.bin\nR_train_coo.row.bin\nR_train_coo.col.bin\nR_train_csr.indptr.bin\nR_train_csr.indices.bin\nR_train_csr.data.bin\nR_train_csc.indptr.bin\nR_train_csc.indices.bin\nR_train_csc.data.bin\n""")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
+print("writing extra meta_modified_all file")
+
+outfile_meta = open("meta_modified_all", 'w')
+outfile_meta.write(str(m) + " " + str(n) + "\n" + str(nnz_train) + "\n")
+outfile_meta.write("""R_train_coo.data.bin
+R_train_coo.row.bin
+R_train_coo.col.bin
+R_train_csr.indptr.bin
+R_train_csr.indices.bin
+R_train_csr.data.bin
+R_train_csc.indptr.bin
+R_train_csc.indices.bin
+R_train_csc.data.bin
+""")
+outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
 
 
 # In[17]:
 
 
-get_ipython().run_cell_magic('time', '', 'print("writing extra meta file")\n\noutfile_meta = open("meta", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n")\noutfile_meta.write(str(nnz_train) + " " + "train.txt\\n")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
+print("writing extra meta file")
+
+outfile_meta = open("meta", 'w')
+outfile_meta.write(str(m) + " " + str(n) + "\n")
+outfile_meta.write(str(nnz_train) + " " + "train.txt\n")
+outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
 
