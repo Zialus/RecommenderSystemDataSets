fd5a459822bccee5aba1d048d7ca12dfad339225
Raul Ferreira <raulmendesferreira@gmail.com>
2019-02-10 16:18:50 +0000

    Trying to run notebooks directly

--- netflix/prepare_netflix_data.py
+++ netflix/prepare_netflix_data.py
@@ -1,21 +1,14 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
 from scipy import sparse
 
 
-# In[ ]:
-
-
-train_data_file = "netflix_mm"
-test_data_file = "netflix_mme"
-
-
-# In[ ]:
+# In[2]:
 
 
 # netflix_mm and netflix_mme should look like this
@@ -34,6 +27,9 @@ n = 17770
 nnz_train = 99072112
 nnz_test = 1408395
 
+train_data_file = "netflix_mm"
+test_data_file = "netflix_mme"
+
 print("preparing test data")
 test_user, test_item, test_rating = np.loadtxt(test_data_file, dtype=np.int32, unpack=True)
 
@@ -41,7 +37,7 @@ print("preparing training data")
 train_user, train_item, train_rating = np.loadtxt(train_data_file, dtype=np.int32, unpack=True)
 
 
-# In[ ]:
+# In[3]:
 
 
 print(test_user)
@@ -65,7 +61,7 @@ print(test_user.size)
 assert test_user.size == nnz_test
 
 
-# In[ ]:
+# In[4]:
 
 
 print(train_user)
@@ -91,7 +87,7 @@ assert np.max(train_user) == m
 assert np.max(train_item) == n
 
 
-# In[ ]:
+# In[5]:
 
 
 R_test_coo = sparse.coo_matrix((test_rating, (test_user, test_item)))
@@ -102,7 +98,7 @@ for i in range(nnz_test):
     outfile_test.write(str(test_user[i]) + " " + str(test_item[i]) + " " + str(test_rating[i]) + "\n")
 
 
-# In[ ]:
+# In[6]:
 
 
 # for test data, we need COO format to calculate test RMSE
@@ -116,7 +112,7 @@ test_row = np.fromfile('R_test_coo.row.bin', dtype=np.int32)
 test_col = np.fromfile('R_test_coo.col.bin', dtype=np.int32)
 
 
-# In[ ]:
+# In[7]:
 
 
 print(R_test_coo.data)
@@ -128,43 +124,67 @@ print(test_row)
 print(test_col)
 
 
-# In[ ]:
+# In[8]:
 
 
-R_train_coo = sparse.coo_matrix((train_rating, (train_user, train_item)))
-assert R_train_coo.nnz == nnz_train
+print(np.max(R_test_coo.data))
+print(np.max(R_test_coo.row))
+print(np.max(R_test_coo.col))
+print("")
+print(np.min(R_test_coo.data))
+print(np.min(R_test_coo.row))
+print(np.min(R_test_coo.col))
+print("")
+print(np.unique(test_user).size)
+print(np.unique(R_test_coo.row).size)
+print(np.unique(test_item).size)
+print(np.unique(R_test_coo.col).size)
 
-outfile_train = open("train.txt", 'w')
-for i in range(nnz_train):
-    outfile_train.write(str(train_user[i]) + " " + str(train_item[i]) + " " + str(train_rating[i]) + "\n")
+
+# In[9]:
 
 
-# In[ ]:
+get_ipython().run_cell_magic('time', '', 'R_train_coo = sparse.coo_matrix((train_rating, (train_user, train_item)))\nassert R_train_coo.nnz == nnz_train\n\noutfile_train = open("train.txt", \'w\')\nfor i in range(nnz_train):\n    outfile_train.write(str(train_user[i]) + " " + str(train_item[i]) + " " + str(train_rating[i]) + "\\n")')
 
 
-# for training data, we need COO format to calculate training RMSE
-# we need CSR format R when calculate X from \Theta
-# we need CSC format of R when calculating \Theta from X
-R_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')
-R_train_coo.row.tofile('R_train_coo.row.bin')
-R_train_coo.col.tofile('R_train_coo.col.bin')
+# In[10]:
 
-train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)
-train_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)
-train_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)
 
-R_train_csr = R_train_coo.tocsr()
-R_train_csc = R_train_coo.tocsc()
+get_ipython().run_cell_magic('time', '', "# for training data, we need COO format to calculate training RMSE\n# we need CSR format R when calculate X from \\Theta\n# we need CSC format of R when calculating \\Theta from X\nR_train_coo.data.astype(np.float32).tofile('R_train_coo.data.bin')\nR_train_coo.row.tofile('R_train_coo.row.bin')\nR_train_coo.col.tofile('R_train_coo.col.bin')\n\nR_train_csr = R_train_coo.tocsr()\nR_train_csc = R_train_coo.tocsc()\n\nR_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')\nR_train_csr.indices.tofile('R_train_csr.indices.bin')\nR_train_csr.indptr.tofile('R_train_csr.indptr.bin')\nR_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')\nR_train_csc.indices.tofile('R_train_csc.indices.bin')\nR_train_csc.indptr.tofile('R_train_csc.indptr.bin')")
 
-R_train_csr.data.astype(np.float32).tofile('R_train_csr.data.bin')
-R_train_csr.indices.tofile('R_train_csr.indices.bin')
-R_train_csr.indptr.tofile('R_train_csr.indptr.bin')
-R_train_csc.data.astype(np.float32).tofile('R_train_csc.data.bin')
-R_train_csc.indices.tofile('R_train_csc.indices.bin')
-R_train_csc.indptr.tofile('R_train_csc.indptr.bin')
 
-train_csc = np.fromfile('R_train_csc.data.bin', dtype=np.float32)
-train_csr = np.fromfile('R_train_csr.data.bin', dtype=np.float32)
+# In[11]:
+
+
+get_ipython().run_cell_magic('time', '', "train_data = np.fromfile('R_train_coo.data.bin', dtype=np.float32)\ntrain_row = np.fromfile('R_train_coo.row.bin', dtype=np.int32)\ntrain_col = np.fromfile('R_train_coo.col.bin', dtype=np.int32)\n\ntrain_csc_data = np.fromfile('R_train_csc.data.bin', dtype=np.float32)\ntrain_csc_indices = np.fromfile('R_train_csc.indices.bin', dtype=np.int32)\ntrain_csc_indptr = np.fromfile('R_train_csc.indptr.bin', dtype=np.int32)\n\ntrain_csr_data = np.fromfile('R_train_csr.data.bin', dtype=np.float32)\ntrain_csr_indices = np.fromfile('R_train_csr.indices.bin', dtype=np.int32)\ntrain_csr_indptr = np.fromfile('R_train_csr.indptr.bin', dtype=np.int32)")
+
+
+# In[12]:
+
+
+print(R_train_coo.data)
+print(R_train_coo.row)
+print(R_train_coo.col)
+print("")
+print(train_data)
+print(train_row)
+print(train_col)
+print("")
+print(R_train_csr.data)
+print(R_train_csr.indices)
+print(R_train_csr.indptr)
+print("")
+print(train_csr_data)
+print(train_csr_indices)
+print(train_csr_indptr)
+print("")
+print(R_train_csc.data)
+print(R_train_csc.indices)
+print(R_train_csc.indptr)
+print("")
+print(train_csc_data)
+print(train_csc_indices)
+print(train_csc_indptr)
 
 
 # In[ ]:
@@ -178,26 +198,6 @@ print(np.min(R_train_coo.data))
 print(np.min(R_train_coo.row))
 print(np.min(R_train_coo.col))
 print("")
-print(R_train_coo.data)
-print(R_train_coo.row)
-print(R_train_coo.col)
-print("")
-print(train_data)
-print(train_row)
-print(train_col)
-print("")
-print(R_train_csr.data)
-print(R_train_csr.indptr)
-print(R_train_csr.indices)
-print("")
-print(train_csr)
-print("")
-print(R_train_csc.data)
-print(R_train_csc.indptr)
-print(R_train_csc.indices)
-print("")
-print(train_csc)
-print("")
 print(np.unique(train_user).size)
 print(np.unique(R_train_coo.row).size)
 print(np.unique(train_item).size)
@@ -207,30 +207,11 @@ print(np.unique(R_train_coo.col).size)
 # In[ ]:
 
 
-print("write extra meta file")
-
-outfile_meta = open("meta_modified_all", 'w')
-outfile_meta.write(str(m) + " " + str(n) + "\n" + str(nnz_train) + "\n")
-outfile_meta.write("""R_train_coo.data.bin
-R_train_coo.row.bin
-R_train_coo.col.bin
-R_train_csr.indptr.bin
-R_train_csr.indices.bin
-R_train_csr.data.bin
-R_train_csc.indptr.bin
-R_train_csc.indices.bin
-R_train_csc.data.bin
-""")
-outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
+get_ipython().run_cell_magic('time', '', 'print("write extra meta file")\n\noutfile_meta = open("meta_modified_all", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n" + str(nnz_train) + "\\n")\noutfile_meta.write("""R_train_coo.data.bin\nR_train_coo.row.bin\nR_train_coo.col.bin\nR_train_csr.indptr.bin\nR_train_csr.indices.bin\nR_train_csr.data.bin\nR_train_csc.indptr.bin\nR_train_csc.indices.bin\nR_train_csc.data.bin\n""")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
 
 
 # In[ ]:
 
 
-print("writing extra meta file")
-
-outfile_meta = open("meta", 'w')
-outfile_meta.write(str(m) + " " + str(n) + "\n")
-outfile_meta.write(str(nnz_train) + " " + "train.txt\n")
-outfile_meta.write(str(nnz_test) + " " + "test.txt\n")
+get_ipython().run_cell_magic('time', '', 'print("writing extra meta file")\n\noutfile_meta = open("meta", \'w\')\noutfile_meta.write(str(m) + " " + str(n) + "\\n")\noutfile_meta.write(str(nnz_train) + " " + "train.txt\\n")\noutfile_meta.write(str(nnz_test) + " " + "test.txt\\n")')
 
